efdc9aa23b7692bbe483f71b9aec95562f6a2da9
ibrahim atif <ibrahimatif455@gmail.com>
2023-05-18 15:42:52 +0300

    book details text and subtext

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -12,8 +13,31 @@ class BookDetailsViewBody extends StatelessWidget {
       children: [
         const CustomBookDetailsAppBar(),
         Padding(
-            padding: EdgeInsets.symmetric(horizontal: size.width * 0.3,),
+            padding: EdgeInsets.symmetric(
+              horizontal: size.width * 0.3,
+            ),
             child: const CustomBookImage()),
+        const SizedBox(
+          height: 43,
+        ),
+        const Text(
+          "The Jungle Book",
+          style: Styles.titleStyle30,
+        ),
+        const SizedBox(
+          height: 6,
+        ),
+        Opacity(
+          opacity: 0.7,
+          child: Text(
+            "Rudyard Kipling",
+            style: Styles.titleStyle18.copyWith(
+                fontWeight: FontWeight.w500, fontStyle: FontStyle.italic),
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
       ],
     );
   }
