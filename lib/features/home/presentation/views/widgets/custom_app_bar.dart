f1f180aa3fa38382264195783bd3a8894eac447b
ibrahim atif <ibrahimatif455@gmail.com>
2023-05-21 23:07:54 +0300

    create custom search for books

--- lib/features/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_app_bar.dart
@@ -1,6 +1,8 @@
+import 'package:bookly/core/utils/app_router.dart';
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({Key? key}) : super(key: key);
@@ -14,10 +16,12 @@ class CustomAppBar extends StatelessWidget {
         children: [
           Image.asset(AssetsData.logo, width: 75, height: 18,),
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.kSearchView);
+            },
             icon: const Icon(
               FontAwesomeIcons.magnifyingGlass,
-              size: 28,
+              size: 22,
             ),
           )
         ],
---
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -0,0 +1,18 @@
+import 'package:bookly/features/search/presentation/views/widgets/custom_search_text_field.dart';
+import 'package:flutter/material.dart';
+
+class SearchViewBody extends StatelessWidget {
+  const SearchViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: const [
+          CustomSearchTextField(),
+        ],
+      ),
+    );
+  }
+}
