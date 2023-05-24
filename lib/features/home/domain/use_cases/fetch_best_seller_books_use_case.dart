8f128fdc3edca481da577dc8ed7dcb1e93b074f3
ibrahim atif <ibrahimatif455@gmail.com>
2023-05-24 15:33:24 +0300

    home use cases refactor

--- lib/features/home/domain/use_cases/fetch_best_seller_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_best_seller_books_use_case.dart
@@ -1,16 +1,16 @@
 import 'package:bookly/core/errors/failure.dart';
+import 'package:bookly/core/use_cases/no_param_use_case.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:bookly/features/home/domain/repository/home_repository.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchBestSellerBooksUseCase
-{
+class FetchBestSellerBooksUseCase extends UseCase<List<BookEntity>> {
   final HomeRepo homeRepo;
 
   FetchBestSellerBooksUseCase(this.homeRepo);
 
-  Future<Either<Failure, List<BookEntity>>> fetchBestSellerBooks()
-  {
-    return homeRepo.fetchBestSellerBooks();
+  @override
+  Future<Either<Failure, List<BookEntity>>> call() async {
+    return await homeRepo.fetchBestSellerBooks();
   }
 }
